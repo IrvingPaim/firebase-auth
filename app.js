d3e8f513f4b3c6d211e2e172c592c89c5d3642e8
Irving Paim <irvingpaim@yahoo.com.br>
2023-08-19 15:45:20 -0300

    adicionando frases

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js'
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js'
+import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js'
 import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js'
 
 const firebaseConfig = {
@@ -16,13 +16,38 @@ const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
 const db = getFirestore(app)
+const collectionPhrases = collection(db, 'phrases')
  
 const buttonGoogle = document.querySelector('[data-js="button-google"]')
 const linkLogout = document.querySelector('[data-js="logout"]')
 
-const addPhrase = e => {
+const addPhrase = async e => {
     e.preventDefault()
-    console.log('callback do envio do form executado')
+
+    try {
+        const addedDoc = await addDoc(collectionPhrases, {
+            movieTitle: DOMPurify.sanitize(e.target.title.value),
+            phrase: DOMPurify.sanitize(e.target.phrase.value)
+        })
+
+        console.log('Document adicionado com o ID:', addedDoc.id)
+
+        e.target.reset()
+
+        const modalAddPhrase = document.querySelector('[data-modal="add-phrase"]')
+        M.Modal.getInstance(modalAddPhrase).close()
+    } catch (error) {
+        console.log('problema na edição do document:', error)
+    }
+
+    /*
+    - obter valores dos inputs
+    - sanitizar valores
+    - criar banco no firestore
+    - adicionar frases no banco
+    - limpar inputs
+    - fechar modal
+    */
 }
 
 const handleAuthStateChanged = user => {
